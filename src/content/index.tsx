0f6edcbe23aac2c58c8fed7952461b0fce9d2860
Vijay Suryawanshi <vijay@classpro.in>
2024-10-02 14:39:23 +0530

    refactore: document ready

--- src/content/index.tsx
+++ src/content/index.tsx
@@ -1,29 +1,31 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+
 import App from './App';
+import { documentReady } from '../utils/documentReady';
 import css from '../index.css?inline';
 
-const init = () => {
+const root = () => {
+  const APP_NAME = 'omnix';
+
   const host = document.createElement('div');
-  host.id = 'omnix-host';
+  host.id = `${APP_NAME}-host`;
   document.body.append(host);
 
   const shadowHost = host.attachShadow({ mode: 'open' });
 
   const root = document.createElement('div');
-  root.id = 'omnix-root';
+  root.id = `${APP_NAME}-root`;
   shadowHost.append(root);
 
-  createRoot(root).render(
+  return root;
+};
+
+documentReady(() => {
+  createRoot(root()).render(
     <StrictMode>
       <style type="text/css">{css}</style>
       <App />
     </StrictMode>,
   );
-};
-
-if (document.readyState === 'complete') {
-  init();
-} else {
-  document.addEventListener('DOMContentLoaded', init);
-}
+});
